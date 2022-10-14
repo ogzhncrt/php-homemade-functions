d89c62b6b7af4bd7b1d9b50ab3a2d0fc176cf50e
Oğuzhan Cerit <ogzhncrt@gmail.com>
2022-10-14 11:10:36 +0200

    Create to_seconds.php

---
+++ to_seconds.php
@@ -0,0 +1,20 @@
+<?php
+    
+    /**
+     * This function calculates seconds in
+     * different type of strings
+     *
+     * @param String $value
+     *
+     * @author ogzhncrt
+     * @return String $seconds
+     *
+     */ 
+    function to_seconds($value){
+        
+
+        return $value;
+    }
+?>
+
+<?php echo to_seconds('12:33:31'); ?>
